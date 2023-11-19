2c30c9657bb802a0af5de0909be97c060c461bb6
MardeevRuslan <118975343+MardeevRuslan@users.noreply.github.com>
2023-11-19 10:05:26 +0700

    Add files via upload

---
+++ Mesh/src/main/java/mesh/Mesh.java
@@ -0,0 +1,16 @@
+package mesh;
+
+public class Mesh {
+    private int length;
+    private int width;
+    private int minStepLength;
+    private int maxStepLength;
+    private int minStepWidth;
+    private int maxStepWidth;
+
+
+
+
+
+
+}
